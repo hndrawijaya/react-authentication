5fc185628c47b4652f4c02db29fed7cbfb2bdead
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-07-10 22:42:08 +0700

    register with firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,16 @@ import { getAuth } from 'firebase/auth';
 
 // TODO: replace with your own config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "mood-meter-13966.firebaseapp.com",
-  projectId: "mood-meter-13966",
-  storageBucket: "mood-meter-13966.appspot.com",
-  messagingSenderId: "17422836348",
-  appId: "1:17422836348:web:25d2fbcb86bbaddb759ff9",
-  measurementId: "G-YKPBJ9XBLH"
+    apiKey: "",
+    authDomain: "mood-meter-13966.firebaseapp.com",
+    projectId: "mood-meter-13966",
+    storageBucket: "mood-meter-13966.appspot.com",
+    messagingSenderId: "17422836348",
+    appId: "1:17422836348:web:25d2fbcb86bbaddb759ff9",
+    measurementId: "G-YKPBJ9XBLH"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+export { auth };
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -6,20 +6,29 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+
+import { auth } from '../config/firebase';
 
 const Register = () => {
     const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState('')
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-        navigate("/");
+        const email = data.get('email');
+        const password = data.get('password');
+        
+        try {
+            const { user } = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(user);
+            navigate("/");
+        } catch (error) {
+            setErrorMessage(error.message);
+        }
     };
 
     return (
@@ -62,6 +71,7 @@ const Register = () => {
                             />
                         </Grid>
                     </Grid>
+                    <Typography color='red'>{errorMessage}</Typography>
                     <Button
                         type="submit"
                         fullWidth

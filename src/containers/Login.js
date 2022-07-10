1f210515015524b227cb4ffa3f2636f329ac66ee
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-07-10 20:02:02 +0700

    able to navigate to home after login/register

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -7,9 +7,11 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate();
+    
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
@@ -17,6 +19,7 @@ const Login = () => {
             email: data.get('email'),
             password: data.get('password'),
         });
+        navigate("/");
     };
 
     return (
